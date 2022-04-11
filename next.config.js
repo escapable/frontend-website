88f5d253a377fd935e9afd896a084ea890b0dfa7
MJ <mj@MJs-MacBook-Air.local>
2022-04-11 19:52:00 -0400

    Added Bing site verification meta tag to the headMetadata.js component.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         "DEV_API_URL": "http://localhost:5100",
         "PRODUCTION_API_URL": "https://www.api.escapable.space",
-        "GOOGLE_VERIFICATION_ID": "g4QY-6RnLaoN2ym4RQDG5Kz4p_54p_LOUVy5c61sL0g"
+        "GOOGLE_VERIFICATION_ID": "g4QY-6RnLaoN2ym4RQDG5Kz4p_54p_LOUVy5c61sL0g",
+        "BING_VERIFICATION_ID": "4CFC932E196C4514125B47370A427E3F"
     }
 }
